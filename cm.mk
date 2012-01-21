8b5aaa6c728cafc038fc577b403177d290d6bffb
Giulio Cervera <giulio.cervera@gmail.com>
2012-01-21 03:59:39 +0100

    shooteru: update fingerprint

--- cm.mk
+++ cm.mk
@@ -8,7 +8,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_shooteru BUILD_ID=GRJ22 BUILD_FINGERPRINT="htc_europe/htc_shooteru/shooteru:2.3.4/GRJ22/105308.2:user/release-keys" PRVIATE_BUILD_DESC="1.20.401.2 CL105308 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_shooteru BUILD_ID=GRJ22 BUILD_FINGERPRINT="htc_europe/htc_shooteru/shooteru:2.3.4/GRJ22/105308.8:user/release-keys" PRVIATE_BUILD_DESC="1.20.401.8 CL105308 release-keys"
 
 PRODUCT_NAME := cm_shooteru
 PRODUCT_DEVICE := shooteru
