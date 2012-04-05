b2a4cc3a2101729a3a61815f8849fa45a72c68db
Giulio Cervera <giulio.cervera@gmail.com>
2012-04-05 15:13:00 +0200

    shooteru: use 540x960 bootanimation

--- cm.mk
+++ cm.mk
@@ -10,5 +10,7 @@ $(call inherit-product, vendor/cm/config/gsm.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_shooteru BUILD_FINGERPRINT="htc_europe/htc_shooteru/shooteru:2.3.4/GRJ22/105308.8:user/release-keys" PRIVATE_BUILD_DESC="1.20.401.8 CL105308 release-keys"
 
+TARGET_BOOTANIMATION_NAME := vertical-540x960
+
 PRODUCT_NAME := cm_shooteru
 PRODUCT_DEVICE := shooteru
