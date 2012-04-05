f48f6bb17b1678b14c0906026af06453a479c6d5
Giulio Cervera <giulio.cervera@gmail.com>
2012-04-06 00:53:36 +0200

    shooteru: use qcom script
    Change-Id: I40d641c7da59b27b4ab7ddfa46b7c2e9c3e48c8c

--- device.mk
+++ device.mk
@@ -34,6 +34,11 @@ PRODUCT_COPY_FILES += \
     device/htc/shooteru/init.shooteru.usb.rc:root/init.shooteru.usb.rc \
     device/htc/shooteru/ueventd.shooteru.rc:root/ueventd.shooteru.rc
 
+# Qualcomm scripts
+PRODUCT_COPY_FILES += \
+    device/htc/shooteru/prebuilt/init.qcom.post_boot.sh:system/etc/init.qcom.post_boot.sh \
+    device/htc/shooteru/prebuilt/init.qcom.efs.sync.sh:system/etc/init.qcom.efs.sync.sh
+
 # BCM4329 BT Firmware
 PRODUCT_COPY_FILES += \
     device/htc/msm8660-common/firmware/bcm4329.hcd:system/vendor/firmware/bcm4329.hcd
