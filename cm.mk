1745182fb0a7519913715478505b824529aef246
Giulio Cervera <giulio.cervera@gmail.com>
2012-05-20 15:34:30 +0200

    shooteru: misc ics update
    * cleanup system.prop
    * fix inclusion order
    * fix mobile interface
    * update fingerprint
    * enable caf overlay
    
    Change-Id: Id35777a23c9531513aeba31c4b9954b94c79f2db

--- cm.mk
+++ cm.mk
@@ -1,14 +1,14 @@
 $(call inherit-product, device/htc/shooteru/full_shooteru.mk)
 
-PRODUCT_RELEASE_NAME := Evo3DGSM
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/gsm.mk)
+PRODUCT_RELEASE_NAME := Evo3DGSM
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_shooteru BUILD_FINGERPRINT="htc_europe/htc_shooteru/shooteru:2.3.4/GRJ22/105308.8:user/release-keys" PRIVATE_BUILD_DESC="1.20.401.8 CL105308 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_shooteru BUILD_FINGERPRINT="htc_europe/htc_shooteru/shooteru:4.0.3/IML74K/351412.1:userdebug/test-keys" PRIVATE_BUILD_DESC="3.08.401.1 CL351412 test-keys"
 
 TARGET_BOOTANIMATION_NAME := vertical-540x960
 
